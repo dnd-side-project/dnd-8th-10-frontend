8ce0b87f39b8b58f86881b91840aa46e9a9d3547
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-01 13:56:09 +0900

    style : 린트설정 변경

--- pages/login/oauth2/callback.tsx
+++ pages/login/oauth2/callback.tsx
@@ -19,14 +19,12 @@ const Login: NextPage = () => {
 		() => oauth2(new URL(document.location.toString()).searchParams.get('code') as string),
 		{
 			onSuccess: (res) => {
-				// eslint-disable-next-line dot-notation
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				const accessToken = (res.headers.get as any)('Authorization').split(' ')[1]; // TODO: 토큰 발췌 방식 바꾸기
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				const refreshToken = (res.headers.get as any)('Refresh').split(' ')[1];
-				localStorage.setItem('ACCESS_TOKEN', accessToken);
-				console.log(accessToken, refreshToken);
-				setCookie('REFRESH_TOKEN', refreshToken, { path: '/', secure: true, sameSite: 'none' });
+				const accessToken = res?.headers['authorization']?.split(' ')[1]; // TODO: 토큰 발췌 방식 바꾸기
+				const refreshToken = res?.headers['refresh']?.split(' ')[1];
+				if (accessToken && refreshToken) {
+					localStorage.setItem('ACCESS_TOKEN', accessToken);
+					setCookie('REFRESH_TOKEN', refreshToken, { path: '/', secure: true, sameSite: 'none' });
+				}
 				// router.push(SERVICE_URL.home); // TODO: 로그인 이전 페이지로 보내기
 			},
 			onError: (error) => {
@@ -38,16 +36,7 @@ const Login: NextPage = () => {
 			refetchOnWindowFocus: false,
 		}
 	);
-	/* useEffect(() => {
-		fetch(
-			`https://wise24life.shop/api/oauth/token?code=${
-				new URL(document.location.toString()).searchParams.get('code') as string
-			}`
-		).then((response) => {
-			console.log(response.headers.get('Authorization')?.split(' ')[1]);
-			console.log(response.headers.get('Refresh'));
-		});
-	}, []); */
-	return <div>OauthCallback</div>;
+
+	return <div />;
 };
 export default Login;
