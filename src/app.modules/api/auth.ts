b1c3e20c797752e1ef53ec4d5a22830d97ced65f
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-07 17:24:53 +0900

    fix : isLocal param 추가

--- src/app.modules/api/auth.ts
+++ src/app.modules/api/auth.ts
@@ -1,7 +1,9 @@
 import client from './client';
 
 export const oauth2 = async (code: string) => {
-	const res = await client.get(`/oauth/token?code=${code}`);
+	const isLocal = Boolean(process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI?.includes('localhost'));
+	console.log('isLocal', isLocal);
+	const res = await client.get(`/oauth/token?code=${code}&isLocal=${isLocal}`);
 	return res;
 };
 
