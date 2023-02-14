69622c4bdc949387a2b42b50c79da8a861eb71b5
Siby1lA <psb4644@gmail.com>
2023-02-14 17:34:10 +0900

    fix: 급여 페이지 매니저 삭제

--- src/app.features/calendar/screens/ManagerScreen.tsx
+++ src/app.features/calendar/screens/ManagerScreen.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SERVICE_URL } from 'src/app.modules/constants/ServiceUrl';
 import { getDaysInMonth } from 'src/app.modules/util/calendar';
 import { getSalaryList } from '../api';
@@ -12,7 +12,6 @@ function ManagerScreen() {
 	const { year, month } = useStore();
 	const router = useRouter();
 	const [salaryData, setSalaryData] = useState<ISalaryList[]>([]);
-	const [manageData, setManageData] = useState<ISalaryList>();
 	const { data, isLoading } = useQuery(
 		['salaryList'],
 		() => getSalaryList({ year: String(year), month: String(month + 1) }),
@@ -29,11 +28,6 @@ function ManagerScreen() {
 			refetchOnWindowFocus: false,
 		}
 	);
-	useEffect(() => {
-		if (manageData === undefined) {
-			setManageData(salaryData.shift());
-		}
-	}, [salaryData]);
 
 	return (
 		<div className="text-[1.5rem]">
@@ -45,26 +39,6 @@ function ManagerScreen() {
 						</div>
 					</div>
 					<div>
-						<div className="mb-[4rem]">
-							<div>매니저</div>
-							<div className="flex justify-between items-center bg-[#F8F8FA] p-[2rem] my-[1rem] rounded-xl">
-								<div className="flex">
-									<div>
-										<div>{manageData?.userName}</div>
-										<div>{getDaysInMonth(year, month)}</div>
-									</div>
-								</div>
-								<div>
-									<span>{manageData?.totalSalary}원</span>
-									<button
-										type="button"
-										onClick={() => router.push(`${SERVICE_URL.calendarSalaryDetail}/${manageData?.userCode}`)}
-									>
-										{'>'}
-									</button>
-								</div>
-							</div>
-						</div>
 						<div>
 							<div>알바생</div>
 							{salaryData.map((data, index) => (
@@ -80,7 +54,10 @@ function ManagerScreen() {
 									</div>
 									<div>
 										<span>{data.totalSalary}</span>
-										<button type="button" onClick={() => router.push(`${SERVICE_URL.calendarSalaryDetail}/${index}`)}>
+										<button
+											type="button"
+											onClick={() => router.push(`${SERVICE_URL.calendarSalaryDetail}/${data.userCode}`)}
+										>
 											{'>'}
 										</button>
 									</div>
