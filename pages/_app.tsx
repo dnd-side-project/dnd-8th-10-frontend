f49d43e01d0f780fca3f4715f1ade903513b2576
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-15 21:17:56 +0900

    fix : font-style 적용안되는 문제 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import '../styles/text.css';
+import '../styles/reset.css';
+import '../styles/checklist.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
