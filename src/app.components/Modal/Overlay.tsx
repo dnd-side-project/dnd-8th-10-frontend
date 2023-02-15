26d779a7e733120cffe142d26e1554d6351aa16f
Siby1lA <psb4644@gmail.com>
2023-02-15 16:54:22 +0900

    feat: 오버레이 블러 추가

--- src/app.components/Modal/Overlay.tsx
+++ src/app.components/Modal/Overlay.tsx
@@ -2,9 +2,10 @@ import useModalStore from 'src/app.modules/store/modal';
 
 interface Props {
 	bgColor?: string;
+	blur?: boolean;
 }
 
-function Overlay({ bgColor = 'bg-transparent-20%' }: Props) {
+function Overlay({ bgColor = 'bg-transparent-20%', blur = false }: Props) {
 	const { modalIsClose } = useModalStore();
 	return (
 		<div>
@@ -12,7 +13,9 @@ function Overlay({ bgColor = 'bg-transparent-20%' }: Props) {
 				type="button"
 				aria-label="button"
 				onClick={() => modalIsClose()}
-				className={`z-999 fixed top-0 left-0 bottom-0 right-0 ${bgColor}`}
+				className={`z-999 fixed top-0 left-0 bottom-0 right-0 ${bgColor} ${
+					blur && 'backdrop-filter backdrop-blur-[0.4rem]'
+				}`}
 			/>
 		</div>
 	);
