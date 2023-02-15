b2e345b5ea019798ecf308c0eb4733d057704ff1
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-15 21:43:49 +0900

    design: 로그인 페이지

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,18 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
 import React from 'react';
-import { KAKAO_OAUTH2_URL } from 'src/app.features/login/constants';
+
+import LoginScreen from 'src/app.features/login/screens/LoginScreen';
 
 function Login() {
-	const router = useRouter();
-
-	return (
-		<div className="w-screen h-screen">
-			<button className="bg-yellow-400 rounded p-2 text-center mx-auto">
-				<a href={KAKAO_OAUTH2_URL}>테스트용 카카오로그인 버튼</a>
-			</button>
-		</div>
-	);
+	return <LoginScreen />;
 }
 
 export default Login;
