37528df442bb84e6ffc49e4d35f4320d2fcc3749
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-16 21:21:21 +0900

    feat : 뒤로가기 버튼 레이아웃 조정

--- src/app.components/Header.tsx
+++ src/app.components/Header.tsx
@@ -10,11 +10,11 @@ function Header({ title, mode }: Props) {
 	const router = useRouter();
 	return (
 		<header
-			className={` h-[5.6rem] fixed flex px-[2rem] max-w-[50rem] -translate-x-[2rem] mx-auto w-full items-center justify-center  z-50 ${
+			className={` h-[5.6rem] fixed flex px-[2rem] max-w-[50rem] -translate-x-[2rem] text-subhead4  mx-auto w-full items-center justify-center  z-50 ${
 				mode === 'white' ? 'text-w' : 'text-g10 bg-w'
 			}`}
 		>
-			<button onClick={() => router.back()} className="absolute left-[2rem] ">
+			<button onClick={() => router.back()} className="absolute left-[2rem] top-[2.1rem]">
 				<BackIcon stroke={`${mode === 'white' ? '#ffffff' : '#66666E'}`} />
 			</button>
 			<h1 className="text-subhead4">{title}</h1>
