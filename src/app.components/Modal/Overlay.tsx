111effa7ffc00b3bdb4725858e15ccba288be024
Siby1lA <psb4644@gmail.com>
2023-02-18 03:17:58 +0900

    fix: 캘린더 최대 너비 변경

--- src/app.components/Modal/Overlay.tsx
+++ src/app.components/Modal/Overlay.tsx
@@ -13,7 +13,7 @@ function Overlay({ bgColor = 'bg-transparent-30%', blur = false }: Props) {
 				type="button"
 				aria-label="button"
 				onClick={() => modalIsClose()}
-				className={`translate-x-0 z-50  fixed max-w-[40rem] mx-auto top-0 left-0 bottom-0 right-0 ${bgColor} ${
+				className={`translate-x-0 z-50  fixed max-w-[42rem] mx-auto top-0 left-0 bottom-0 right-0 ${bgColor} ${
 					blur && 'backdrop-filter backdrop-blur-[0.4rem]'
 				}`}
 			/>
