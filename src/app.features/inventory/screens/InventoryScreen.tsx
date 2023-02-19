af0f852fe51bdd84394e6458aa8cb30955334324
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-20 04:22:04 +0900

    design : 시재 기록 개요

--- src/app.features/inventory/screens/InventoryScreen.tsx
+++ src/app.features/inventory/screens/InventoryScreen.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import Header from 'src/app.components/Header';
+import ProfileImage from 'src/app.components/ProfileImage';
 import { IInventoryHistory } from 'src/app.modules/api/inventory';
 import { SERVICE_URL } from 'src/app.modules/constants/ServiceUrl';
 
@@ -25,13 +26,12 @@ function InventoryScreen({ inventoryHistory }: Props) {
 			url: SERVICE_URL.inventoryCard,
 		},
 	];
-	const list = [];
 
 	return (
 		<>
 			<Header title="시재점검" />
 
-			<main className="pt-[5.6rem]">
+			<main className="pt-[5.6rem] h-[100vh]">
 				<section className="py-[1.6rem]">
 					<div className="p-[1.6rem] space-y-[1.6rem] bg-g1 rounded-[0.8rem]">
 						<div className="flex items-center justify-between text-subhead1">
@@ -40,7 +40,20 @@ function InventoryScreen({ inventoryHistory }: Props) {
 								<span className="text-primary ">전체보기</span>
 							</Link>
 						</div>
-						{list.length ? <div className="" /> : null}
+						{inventoryHistory.length ? (
+							<ul className="space-y-[0.8rem] ">
+								{inventoryHistory?.slice(0, 2).map((item) => (
+									<li className="flex items-center justify-between ">
+										<div className="flex items-center space-x-[0.8rem]">
+											<ProfileImage userProfileCode={item.userProfileCode} />
+											<span className="text-g9 text-subhead2">{item.userName}</span>
+											<span className="text-g6 text-subhead1">{item.workTime}</span>
+										</div>
+										<span className="text-g9 text-subhead1">{item.list[0].inventoryName}</span>
+									</li>
+								))}
+							</ul>
+						) : null}
 					</div>
 				</section>
 				<div className="bg-g1 w-[calc(100%+4rem)] -translate-x-[2rem] h-[1.2rem]" />
