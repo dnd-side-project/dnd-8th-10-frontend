f7e61bedfc246d8eadc9a574b8065c1478cb65a2
Siby1lA <psb4644@gmail.com>
2023-02-20 20:51:43 +0900

    fix: 유저 정보 컴포넌트 마이 페이지일 시

--- src/app.components/Profile/index.tsx
+++ src/app.components/Profile/index.tsx
@@ -3,26 +3,30 @@ import ProfileImage from '../ProfileImage';
 interface Props {
 	userProfileCode: number;
 	userName: string;
-	role: string;
+	role?: string;
 	workTime: string;
+	mypage?: boolean;
 }
-function Profile({ userData }: { userData: Props }) {
+function Profile({ mypage = true, userProfileCode, userName, role, workTime }: Props) {
 	return (
 		<div className="flex items-center">
-			<ProfileImage round userProfileCode={userData.userProfileCode} />
+			<ProfileImage round userProfileCode={userProfileCode} />
 			<div className="ml-[1.6rem]">
-				<span className="text-w text-subhead4">{userData.userName}</span>
-				<span className="text-g3 text-subhead3 ml-[0.8rem]">{userData.role === 'MANAGER' ? '점장' : '알바생'}</span>
+				<span className={`${mypage ? 'text-g10' : 'text-w'} text-subhead4`}>
+					{userName}
+					{mypage && '님'}
+				</span>
+				{role && <span className="text-g3 text-subhead3 ml-[0.8rem]">{role === 'MANAGER' ? '점장' : '알바생'}</span>}
 				<div className="flex">
-					{userData.workTime.includes(',') ? (
-						userData.workTime.split(',').map((work, index) => (
+					{workTime.includes(',') ? (
+						workTime.split(',').map((work, index) => (
 							<div key={index} className="bg-g9 w-fit rounded-[0.4rem] py-[0.2rem] px-[0.8rem] mt-[0.8rem] mr-[0.8rem]">
 								<span className="text-w text-body1">{work}</span>
 							</div>
 						))
 					) : (
 						<div className="bg-g9 w-fit rounded-[0.4rem] py-[0.2rem] px-[0.8rem] mt-[0.8rem]">
-							<span className="text-w text-body1">{userData.workTime.split(',')[0]}</span>
+							<span className="text-w text-body1">{workTime}</span>
 						</div>
 					)}
 				</div>
