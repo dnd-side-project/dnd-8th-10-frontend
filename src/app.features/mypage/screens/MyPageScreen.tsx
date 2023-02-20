c95df04a4ee113cbc1c5fba21f706c165d05c493
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-20 20:12:15 +0900

    design : mypage

--- src/app.features/mypage/screens/MyPageScreen.tsx
+++ src/app.features/mypage/screens/MyPageScreen.tsx
@@ -3,6 +3,8 @@ import Divider from 'src/app.components/Divider';
 import InfoBox from 'src/app.components/InfoBox';
 import ProfileImage from 'src/app.components/ProfileImage';
 import SettingIcon from 'src/app.modules/assets/mypage/setting.svg';
+import StoreIcon from 'src/app.modules/assets/mypage/store.svg';
+import LeftArrowIcon from 'src/app.modules/assets/mypage/arrowLeft.svg';
 import { getSplittedWorkPlaceString } from 'src/app.modules/util/getSplittedWorkPlaceString';
 
 interface IUser {
@@ -26,12 +28,27 @@ function MyPageScreen({ user }: Props) {
 				</button>
 			</header>
 			<main className="h-[100vh] pt-[7.2rem]">
-				<div>
-					<InfoBox>{getSplittedWorkPlaceString(user?.workPlace ?? '')}</InfoBox>
+				<div className="pb-[1.6rem]">
+					<InfoBox className="flex items-center justify-between">
+						<div className="flex items-center space-x-[0.8rem]">
+							<StoreIcon />
+							<span className="text-subhead1 text-g9">{getSplittedWorkPlaceString(user?.workPlace ?? '')}</span>
+						</div>
+						<LeftArrowIcon />
+					</InfoBox>
 				</div>
 				<Divider />
-				<div>
-					<ul />
+				<div className="py-[2.4rem]">
+					<ul className=" space-y-[2rem]">
+						<li className=" border-solid border-b-[0.1rem] pb-[2rem] border-g3 w-full flex flex-col space-y-[0.2rem] ">
+							<span className="text-subhead1 text-g6">게시물 관리</span>
+							<span className="text-subhead2 text-g9">내가 쓴글</span>
+						</li>
+						<li className=" border-solid border-b-[0.1rem] pb-[2rem] border-g3 w-full flex flex-col space-y-[0.2rem] ">
+							<span className="text-subhead1 text-g6">고객센터</span>
+							<span className="text-subhead2 text-g9">문의하기</span>
+						</li>
+					</ul>
 				</div>
 			</main>
 		</>
