eab7dd0dfe3bb99dfaeebb78375a6ccf4eccee97
Siby1lA <psb4644@gmail.com>
2023-02-20 18:20:19 +0900

    design: 모달 애니메이션

--- src/app.components/Modal/Overlay.tsx
+++ src/app.components/Modal/Overlay.tsx
@@ -1,22 +1,37 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 import useModalStore from 'src/app.modules/store/modal';
 
 interface Props {
 	bgColor?: string;
 	blur?: boolean;
+	children: ReactElement;
 }
 
-function Overlay({ bgColor = 'bg-transparent-30%', blur = false }: Props) {
-	const { modalIsClose } = useModalStore();
+function Overlay({ children, bgColor = 'bg-transparent-30%', blur = false }: Props) {
+	const { modalIsClose, isModalOpen } = useModalStore();
+	const [isAnimating, setIsAnimating] = useState(false);
+	useEffect(() => {
+		if (isModalOpen) {
+			setIsAnimating(true);
+		}
+	}, [isModalOpen]);
 	return (
 		<div>
 			<button
 				type="button"
 				aria-label="button"
-				onClick={() => modalIsClose()}
+				onClick={() => {
+					setIsAnimating(false);
+					setTimeout(() => {
+						modalIsClose();
+					}, 500);
+				}}
 				className={`translate-x-0 z-50  fixed max-w-[42rem] mx-auto top-0 left-0 bottom-0 right-0 ${bgColor} ${
 					blur && 'backdrop-filter backdrop-blur-[0.4rem]'
-				}`}
+				} transition-all duration-500 ease-in-out ${isAnimating ? 'opacity-100' : 'opacity-0'}`}
 			/>
+			{React.cloneElement(children, { isAnimating, setIsAnimating })}
 		</div>
 	);
 }
--- src/app.components/Modal/TopModal.tsx
+++ src/app.components/Modal/TopModal.tsx
@@ -1,22 +1,88 @@
+import { useEffect, useRef, useState } from 'react';
 import useModalStore from 'src/app.modules/store/modal';
 
 interface Props {
 	children: React.ReactElement;
 	bgColor?: string;
+	isAnimating?: boolean;
+	setIsAnimating?: (isAnimating: boolean) => void;
 }
 
-function TopModal({ children, bgColor = 'bg-w' }: Props) {
-	const { modalIsClose } = useModalStore();
+function TopModal({ children, bgColor = 'bg-w', isAnimating, setIsAnimating }: Props) {
+	const { modalIsClose, isModalOpen } = useModalStore();
+	const [height, setHeight] = useState<number>(0);
+	const [isDragging, setIsDragging] = useState(false);
+	const [dragStartY, setDragStartY] = useState(0);
+	const [dragOffsetY, setDragOffsetY] = useState(0);
+	const modalRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (isModalOpen) {
+			setIsAnimating && setIsAnimating(true);
+		}
+		const _ = setTimeout(() => {
+			if (modalRef.current) {
+				setHeight(modalRef.current.getBoundingClientRect().height + 36);
+			}
+			clearTimeout(_);
+		}, 100);
+	}, [isModalOpen]);
+
+	useEffect(() => {
+		const modal = modalRef.current;
+		if (modal && isDragging) {
+			const handleMouseMove = (event: MouseEvent) => {
+				setDragOffsetY(Math.max(event.pageY - dragStartY, 0));
+			};
+
+			const handleMouseUp = () => {
+				setIsDragging(false);
+				if (dragOffsetY > 50) {
+					setIsAnimating && setIsAnimating(false);
+					setTimeout(() => {
+						modalIsClose();
+					}, 500);
+				} else {
+					setDragOffsetY(0);
+				}
+			};
+
+			document.addEventListener('mousemove', handleMouseMove);
+			document.addEventListener('mouseup', handleMouseUp);
+
+			return () => {
+				document.removeEventListener('mousemove', handleMouseMove);
+				document.removeEventListener('mouseup', handleMouseUp);
+			};
+		}
+	}, [isDragging, dragStartY, dragOffsetY, modalIsClose, height]);
+
+	const handleMouseDown = (event: React.MouseEvent) => {
+		event.preventDefault();
+		setIsDragging(true);
+		setDragStartY(event.pageY);
+	};
+
 	return (
 		<div
-			className={`${bgColor} rounded-t-[1.6rem] z-50 fixed bottom-0 max-w-[42rem] -translate-x-[2rem] mx-auto w-full`}
+			className={`${bgColor} h-full rounded-t-[1.6rem] z-50 fixed bottom-0 max-w-[42rem] -translate-x-[2rem] mx-auto w-full transition-transform duration-500 ease-in-out ${
+				isAnimating ? 'transform translate-y-0' : 'transform translate-y-full'
+			}`}
+			style={{ height: `calc(${height}px - ${dragOffsetY}px)` }}
 		>
-			<div className="flex justify-center mt-[0.8rem] mb-[2.4rem]">
-				<button type="button" onClick={() => modalIsClose()} className="w-[5rem] h-[0.4rem] bg-g4 rounded-[1rem]">
-					{' '}
-				</button>
+			<div className="flex justify-center mt-[0.8rem] mb-[2.4rem]" onMouseDown={handleMouseDown}>
+				<button
+					type="button"
+					onClick={() => {
+						setIsAnimating && setIsAnimating(false);
+						setTimeout(() => {
+							modalIsClose();
+						}, 500);
+					}}
+					className="w-[5rem] h-[0.4rem] bg-g4 rounded-[1rem]"
+				/>
 			</div>
-			<div className="px-[2rem] pb-[2rem]">
+			<div ref={modalRef} className="px-[2rem] pb-[2rem]">
 				<div>{children}</div>
 			</div>
 		</div>
