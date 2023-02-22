c8cdad8ece966d33f1cb0c995470557ac4952edf
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-23 00:05:42 +0900

    refactor: 회원등록시 유저정보 렌더링 최적화
    - 로그인할때 토큰 발급 api에서 전달 받은 유저네임을 회원등록 페이지 url에 실어서 . useUser 가 data를 받아오기 전에 사용
    - 회원등록 완료 페이지도 useUser가 데이터를 받아오기 전에 기존에 recoil에 있던 정보를 사용하도록 함

--- pages/inventory/index.tsx
+++ pages/inventory/index.tsx
@@ -3,6 +3,7 @@ import InventoryScreen from 'src/app.features/inventory/screens/InventoryScreen'
 import useInventoryHistory from 'src/app.modules/hooks/inventory/useInventoryHistory';
 
 // TODO: 인벤토리 폴더 구조 변경 필요. 겹치는 코드 많음
+// TODO: 여기서 하위 시재점검 페이지들 preFetching 필요
 function Inventory() {
 	const { data } = useInventoryHistory();
 	return <InventoryScreen inventoryHistory={data} />;
