dff457a07649834944c92d5a2f882b02f7e1735c
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-22 22:34:43 +0900

    feat : 토큰 존재할때만 백에 유저 정보 요청

--- src/app.modules/hooks/user/useUser.ts
+++ src/app.modules/hooks/user/useUser.ts
@@ -1,19 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUser } from 'src/app.modules/api/user';
+import { getCookie } from 'src/app.modules/cookie';
 
 function useUser() {
 	const { data, refetch, isLoading } = useQuery(['user', 'me'], getUser, {
 		select: (res) => res.data.data,
 		onSuccess: (res) => {
-			console.log(res);
+			console.log('유저 정보', res);
 		},
 		onError: (error) => {
 			console.log(error);
 		},
-		retry: false,
-		refetchOnMount: false,
-		refetchOnReconnect: false,
-		refetchOnWindowFocus: false,
+		suspense: true,
+		enabled: Boolean(getCookie('ACCESS_TOKEN')) || Boolean(getCookie('REFRESH_TOKEN')), // 토큰이 없으면 유저상태를 가져 올 수 없음. 비로그인 상태로 전환됨
 	});
 	return { data, refetch, isLoading };
 }
