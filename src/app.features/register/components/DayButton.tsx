e745ab7e8e0f63a6e0e7e5daeab66d2e39421a13
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-02-26 21:32:03 +0900

    design : 요일선택버튼 컬러 변경

--- src/app.features/register/components/DayButton.tsx
+++ src/app.features/register/components/DayButton.tsx
@@ -10,16 +10,17 @@ interface Props {
 
 function Calendar({ value, item, onClick, state, name }: Props) {
 	console.log(state);
-	const getBorderColor = () => {
-		if (state === 'default') return 'border-g3';
-		return 'border-g9';
+	const getBorderStyle = () => {
+		return `${state === 'default' ? 'border-g3 border-[0.15rem]' : ''}`;
 	};
 	const getTextColor = () => {
 		if (state === 'focus') return 'text-white';
-		return 'text-g9';
+		if (state === 'default') return 'text-g9';
+		return 'text-primary';
 	};
 	const getBgColor = () => {
-		if (state === 'focus') return 'bg-g9';
+		if (state === 'focus') return 'bg-primary';
+		if (state === 'selected') return 'bg-primarySub';
 		return 'bg-w';
 	};
 	return (
@@ -27,7 +28,7 @@ function Calendar({ value, item, onClick, state, name }: Props) {
 			name={name}
 			value={value}
 			onClick={onClick}
-			className={`px-[1.05rem] py-[0.7rem] rounded-[0.8rem] border-[0.15rem] ${getBgColor()}  ${getBorderColor()} ${getTextColor()} `}
+			className={`w-[3.4rem] text-subhead2 h-[3.4rem]  rounded-[0.8rem]   ${getBgColor()}  ${getBorderStyle()} ${getTextColor()} `}
 		>
 			{item}
 		</button>
