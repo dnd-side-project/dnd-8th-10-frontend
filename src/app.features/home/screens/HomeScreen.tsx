44215c6a8ddfd5195441868f07fa609e235cbf2f
Siby1lA <psb4644@gmail.com>
2023-02-26 13:26:02 +0900

    fix: BoardPreview 오타 수정

--- src/app.features/home/screens/HomeScreen.tsx
+++ src/app.features/home/screens/HomeScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MutateTpye } from 'src/app.modules/api/client';
 import { MutateBody } from 'src/app.features/calendar/api';
 import WorkStatus from '../components/WorkStatus';
-import BoardPrivew from '../components/BoardPrivew';
+import BoardPreview from '../components/BoardPreview';
 import MainPoster from '../components/MainPoster';
 import Shortcut from '../components/Shortcut';
 
@@ -19,7 +19,7 @@ function HomeScreen({ grayData, WorkMutate, todayWork, userData }: Props) {
 	return (
 		<div className="bg-[#FCFCFF] w-[calc(100%+4rem)] px-[2rem] -translate-x-[2rem] py-[5.6rem]">
 			<MainPoster />
-			<BoardPrivew />
+			<BoardPreview />
 			<WorkStatus grayData={grayData} WorkMutate={WorkMutate} todayWork={todayWork} userName={userData.userName} />
 			<Shortcut />
 		</div>
