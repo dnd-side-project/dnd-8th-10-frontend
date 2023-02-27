86f63799a844e8992b79779bd3a38c32cf2b9163
Siby1lA <psb4644@gmail.com>
2023-02-28 04:21:49 +0900

    chore: import 이후 빈줄 추가

--- pages/board/write.tsx
+++ pages/board/write.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import React from 'react';
 import BoardWriteScreen from 'src/app.features/board/screens/BoardWriteScreen';
+
 const WritePage: NextPage = () => {
 	return (
 		<div className="h-[100vh] mx-auto relative w-full">
