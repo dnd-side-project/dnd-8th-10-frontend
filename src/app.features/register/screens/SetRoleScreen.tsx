45c6147c0778606bd7ddf65dbd877293802471bc
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-01 17:22:40 +0900

    feat : 회원등록 role 선택 lottie 애니메이션 추가

--- src/app.features/register/screens/SetRoleScreen.tsx
+++ src/app.features/register/screens/SetRoleScreen.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ActiveWorkerSvg from 'src/app.modules/assets/register/active_worker.svg';
 import ActiveManagerSvg from 'src/app.modules/assets/register/active_manager.svg';
 import InActiveWorkerSvg from 'src/app.modules/assets/register/inactive_worker.svg';
 import InActiveManagerSvg from 'src/app.modules/assets/register/inactive_manager.svg';
 import { RoleType } from 'src/app.modules/api/user';
+import workerJson from 'public/lottie/worker.json';
+import managerJson from 'public/lottie/manager.json';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import lottie from 'lottie-web';
 import RegisterLayout from '../components/RegisterLayout';
 import useRegisterUserStore from '../store';
 
+function Jsontest1() {
+	useEffect(() => {
+		const container = document.querySelector('#container');
+		if (!container) return;
+		lottie.loadAnimation({
+			container,
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			animationData: workerJson,
+		});
+	}, []);
+	return <div id="container" />;
+}
+
+function Jsontest2() {
+	useEffect(() => {
+		const container = document.querySelector('#container2');
+		if (!container) return;
+		lottie.loadAnimation({
+			container,
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			animationData: managerJson,
+		});
+	}, []);
+	return <div id="container2" />;
+}
 interface Props {
 	userName: string;
 }
@@ -20,6 +53,7 @@ function SetRoleScreen({ userName }: Props) {
 	const roleHandler = (value: RoleType) => {
 		setRole(value);
 	};
+
 	// TODO: 화면크기별로 버튼 간격 대응하기
 	return (
 		<RegisterLayout curPage={1} canGoNext={role !== null}>
@@ -29,6 +63,7 @@ function SetRoleScreen({ userName }: Props) {
 					<br />
 					어떤일을 하고 계신가요?
 				</h1>
+
 				<div className="fixed flex max-w-[38rem] mx-auto justify-between w-[calc(100vw-4rem)] top-[20vh] ">
 					<button
 						type="button"
@@ -38,7 +73,7 @@ function SetRoleScreen({ userName }: Props) {
 						className="aria-pressed:text-g9 aria-pressed:text-subhead4 text-g7 text-body3 flex flex-col items-center space-y-[0.8rem]"
 					>
 						<div className="shadow-gray  rounded-[0.8rem]">
-							{role === 'WORKER' ? <ActiveWorkerSvg /> : <InActiveWorkerSvg />}
+							{role === 'WORKER' ? <Jsontest1 /> : <InActiveWorkerSvg />}
 						</div>
 						<span>알바생</span>
 					</button>
@@ -50,7 +85,7 @@ function SetRoleScreen({ userName }: Props) {
 						className="aria-pressed:text-g9 aria-pressed:text-subhead4 text-g7 text-body3 flex flex-col items-center space-y-[0.8rem]"
 					>
 						<div className="shadow-gray rounded-[0.8rem]">
-							{role === 'MANAGER' ? <ActiveManagerSvg /> : <InActiveManagerSvg />}
+							{role === 'MANAGER' ? <Jsontest2 /> : <InActiveManagerSvg />}
 						</div>
 						<span>점장</span>
 					</button>
