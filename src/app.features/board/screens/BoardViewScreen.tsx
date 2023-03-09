44514835c9d1fb469629543b158b33e7a17f4739
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-09 23:12:07 +0900

    feat :댓글 작성 p태그에 contentEditable 속성 부여하는 방식으로 변경

--- src/app.features/board/screens/BoardViewScreen.tsx
+++ src/app.features/board/screens/BoardViewScreen.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 import { useRouter } from 'next/router';
-import React, { BaseSyntheticEvent, MouseEventHandler, useEffect, useRef, useState } from 'react';
+import React, { BaseSyntheticEvent, useEffect, useRef, useState } from 'react';
 import BoardModal from 'src/app.components/Modal/BoardModal';
 import Modal from 'src/app.components/Modal/Modal';
 import Overlay from 'src/app.components/Modal/Overlay';
@@ -34,8 +34,8 @@ interface Props {
 	PutCommentMutate: MutateTpye<PutCommentBody>;
 }
 // TODO: 게시물 확인 버튼 api 연동(0)
-// TODO: 백엔드 boardViewData에 해당 게시물을 체크했는지에 대한 필드 추가 됬는지 확인하기
-// TODO: 댓글 멘션 관련 로직 논의
+// TODO: 백엔드 boardViewData에 해당 게시물을 체크했는지에 대한 필드 추가 됬는지 확인하기 (0)
+// TODO: 댓글 멘션 관련 로직 논의 (0)
 // TODO: 댓글 C(0),R(0),U(0),D(0)
 function BoardViewScreen({
 	userData,
@@ -77,14 +77,13 @@ function BoardViewScreen({
 	// console.log(boardViewData);
 	const mentionRef = useRef<HTMLDivElement>(null);
 	const [commentSortBy, setCommentSortBy] = useState<SoryByType>('earliest');
-	const [newComments, setNewComments] = useState<string[]>(['']); // 초기화
 	const [commentInputMode, setCommentInputMode] = useState<'small' | 'wide'>('small');
 	const [focusComment, setFocusComment] = useState<FocusCommentType | null>(null);
 	const [isMyPost, setIsMyPost] = useState<boolean>(false);
 	const [storeFecthDisabled, setStoreFetchDisabled] = useState<boolean>(true);
 	const [mentionUserCodes, setMentionUserCodes] = useState<string[]>([]);
 	const { data: storeInfo, refetch: storeRefetch } = useStore(true);
-	const NEW_COMMENT_LAST_INDEX = newComments.length - 1;
+	const commentFormRef = useRef<HTMLDivElement>(null);
 	const commentSortHandler = (sortBy: SoryByType) => {
 		setCommentSortBy(sortBy);
 	};
@@ -99,13 +98,10 @@ function BoardViewScreen({
 		if (commentSortBy === 'earliest') return comments;
 		return [...comments].reverse();
 	};
-	// console.log(newComments);
-	const newCommentHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const { value } = e.target;
+	const newCommentHandler = (e: React.ChangeEvent<HTMLDivElement>) => {
 		// TODO: 멘션 로직 분리
 		// metion 기능 최초실행시 fetch On
-		//	console.log(NEW_COMMENT_LAST_INDEX, value, value.slice(-1), value.slice(-1) === '@', '골뱅인데...');
-		if (value.slice(-1) === '@') {
+		if (e.target.innerText.slice(-1) === '@') {
 			if (storeFecthDisabled) {
 				console.log('골뱅이');
 				setStoreFetchDisabled(false);
@@ -113,140 +109,22 @@ function BoardViewScreen({
 				storeRefetch();
 			}
 			openMentionModal();
+		} else if (isMentionModalOpen) {
+			closeMentiondModal();
 		}
-		if (mentionRef?.current === null) return;
-		// 댓글 지워서 , 마지막 인덱스 value 크기 0됨. 앞 인덱스로 이동해야함
-		if (value.length === 0) {
-			if (newComments.length === 1) {
-				setNewComments(['']); // 초기화
-				mentionRef.current.innerHTML = '';
-				return;
-			}
-			const prev = [...newComments];
-			console.log(NEW_COMMENT_LAST_INDEX, 'asdf');
-			prev.pop();
-			console.log(prev, 'prev');
-
-			if (prev.slice(-1)[0].includes('<ws-mention class="mention-text"')) {
-				console.log('is metion');
-				// @userName 발췌
-				const mentionTag = prev.pop() as string;
-				const tmp = mentionTag.split('</ws-mention>').slice(-2, -1)[0];
-				const userName = tmp.split('@').slice(-1)[0];
-				console.log(userName, 'mentionString');
-				prev.push(`@${userName}`);
-			}
-
-			setNewComments(prev);
-			mentionRef.current.innerHTML = `${prev.join('')}`;
-			// mentionRef.current.innerHTML = `${prev.join('')}`;
-			return;
-		}
-
-		// 마지막 index에서 수정 작업
-		const prev = [...newComments];
-		console.log(NEW_COMMENT_LAST_INDEX, 'asdf');
-		prev[NEW_COMMENT_LAST_INDEX] = value;
-		console.log(prev);
-
-		setNewComments(prev);
-		mentionRef.current.innerHTML = `${prev.join('')}`;
-
-		/* if (value.length < newComment.length && newComment.length) {
-			console.log(1);
-			const tmp = `${(mentionRef.current.innerHTML as string).slice(
-				0,
-				(mentionRef.current.innerHTML as string).length - (newComment.length - value.length)
-			)}`;
-			if (value.length) {
-				// 멘션지점일 경우
-				console.log(2);
-
-				if (tmp.split('<span').slice(-1).toString().includes('class="mention-text"') && tmp.endsWith('</span')) {
-					console.log(3);
-					tmpCommentRef.current = '';
-					const lastMetionToText = value;
-					console.log('멘션지점임22', `${tmp.split('<span').slice(0, -1).join('<span')}`, lastMetionToText);
-
-					setNewComment(lastMetionToText);
-					// console.log(, 'test');
-					// mentionRef.current.innerHTML = tmp;
-					mentionRef.current.innerHTML = `${tmp.split('<span').slice(0, -1).join('<span')}${lastMetionToText}`;
-					tmpCommentRef.current = tmp.split('<span').slice(0, -1).join('<span');
-				} else {
-					console.log(4, value, value.length, newComment.length);
-					setNewComment(value);
-					if (tmpCommentRef.current === '' && mentionRef.current.innerHTML) {
-						console.log(mentionRef.current.innerHTML, value, '4-1');
-						tmpCommentRef.current = `${mentionRef.current.innerHTML ?? ''}`;
-					}
-					mentionRef.current.innerHTML = `${tmpCommentRef.current}${value}`;
-				}
-			}
-			if (value.length === 0) {
-				console.log(5);
-				if (tmp === '') {
-					setNewComment('');
-
-					mentionRef.current.innerHTML = ``;
-					tmpCommentRef.current = mentionRef.current.innerHTML;
-					return;
-				}
-				console.log(tmp, 'tmp', tmp.split('<span'), tmp.split('<span').slice(-1).toString());
-				// 멘션지점일 경우
-				if (tmp.split('<span').slice(-1).toString().includes('class="mention-text"') && tmp.endsWith('</span>')) {
-					console.log(6);
-					const lastMetionToText = `@${tmp.split('</span>').slice(-2)[0].split('@').slice(-1).join()}`;
-					setNewComment(lastMetionToText);
-					console.log('멘션지점임', 3, lastMetionToText);
-					// console.log(, 'test');
-					mentionRef.current.innerHTML = tmp;
-					// mentionRef.current.innerHTML = `${tmp.split('<span').slice(0, -1).join('<span')}<span>${lastMetionToText}`;
-					tmpCommentRef.current = tmp;
-				} else {
-					console.log(7);
-					tmpCommentRef.current = '';
-					const lastMetionToText = `${tmp.split('</span>').slice(-1)[0]}`;
-					// 멘션 지점 아님
-					console.log(
-						'멘션지점 아님',
-						tmp,
-						lastMetionToText.length,
-						`${tmp.split('</span>').slice(0, -1).join('</span>')}</span>`,
-						'ㅅㄷㅅㄷㅅㄷ'
-					);
-					setNewComment(lastMetionToText);
-					mentionRef.current.innerHTML =
-						tmp.split('</span>').length === 1
-							? tmp
-							: `${tmp.split('</span>').slice(0, -1).join('</span>')}</span>${lastMetionToText}`;
-					tmpCommentRef.current = mentionRef.current.innerHTML;
-				}
-			}
-		} else {
-			console.log(6);
-			console.log(tmpCommentRef);
-			if (tmpCommentRef.current === '' && mentionRef.current.innerHTML) {
-				console.log(mentionRef.current.innerHTML, value, '입장3');
-				// tmpCommentRef.current = `${mentionRef.current.innerHTML ?? ''}${value}`;
-			}
-			setNewComment(value);
-			mentionRef.current.innerHTML = `${tmpCommentRef.current}${value}`;
-		} */
 	};
 
 	const newCommentSubmitHandler = () => {
-		/* console.log(newComments, '댓글 post');
 		const { postId } = boardViewData;
 		// const content = newComments.join().trim();
-		if (!newComment.trim() || !postId) return;
-		console.log('adsfafd');
+		if (!postId) return;
+		if (mentionRef?.current === null) return;
 		const mentionUserCodesBody = Array.from(new Set(mentionUserCodes));
-		const body = { postId, content: mentionRef.current?.innerHTML as string, userCode: mentionUserCodesBody };
-		console.log(body, 'bodybody');
+		const body = { postId, content: mentionRef.current.innerHTML as string, userCode: mentionUserCodesBody };
+		console.log(body, 'post comment body');
 		PostCommentMutate(body);
-		setNewComment('');
-		setMentionUserCodes([]); */
+
+		setMentionUserCodes([]);
 	};
 	const deleteCommentHandler = (commentId: number) => {
 		console.log('삭제', commentId);
@@ -277,22 +155,35 @@ function BoardViewScreen({
 	// 멘션할 유저 선택함
 	const mentionUserHandler = (userCode: string, userName: string) => {
 		if (mentionRef?.current === null) return;
+		if (commentFormRef?.current === null) return;
 		const userNameString = `@${userName}`;
 		// 코멘트 arr size 2 증가
 		setMentionUserCodes((prev) => [...prev, userCode]);
 		const newComment = `<ws-mention class="mention-text" data-index=${mentionUserCodes.length}>${userNameString}</ws-mention>`;
-		const prev = [...newComments];
-		prev[NEW_COMMENT_LAST_INDEX] = prev[NEW_COMMENT_LAST_INDEX].slice(0, -1);
-		setNewComments([...prev, newComment, ' ']);
+		// const prev = [...newComments];
+		// prev[NEW_COMMENT_LAST_INDEX] = prev[NEW_COMMENT_LAST_INDEX].slice(0, -1);
+		// setNewComments([...prev, newComment, ' ']);
 
-		mentionRef.current.innerHTML = `${prev.join('')}${newComment}`;
+		mentionRef.current.innerHTML = `${mentionRef.current.innerHTML.slice(0, -1)}${newComment}&nbsp;`;
+		const selection = window.getSelection();
+		console.log(window.getSelection()?.toString(), 'qqqq');
+		const newRange = document.createRange();
+		console.log(newRange);
+		newRange.selectNodeContents(mentionRef.current);
+		newRange.collapse(false);
+		console.log(newRange.endContainer);
+		// newRange.setEnd(end);
+		selection?.removeAllRanges();
+		selection?.addRange(newRange);
 		setCommentInputMode('wide');
 		closeMentiondModal();
 	};
-	const mentionUserList = () =>
-		storeInfo?.userList?.filter(({ userName }: IBoardCheckPerson) => {
-			return userName.includes(newComments[NEW_COMMENT_LAST_INDEX].split('@').slice(-1).toString());
-		});
+	// TODO: 필터 적용하기
+	const mentionUserList = () => storeInfo?.userList;
+	// const mentionUserList = () =>
+	//	storeInfo?.userList?.filter(({ userName }: IBoardCheckPerson) => {
+	//		return userName.includes(newComments[NEW_COMMENT_LAST_INDEX].split('@').slice(-1).toString());
+	//	});
 
 	//= useEffect(() => {
 	//	if (mentionRef?.current === null) return;
@@ -308,9 +199,17 @@ function BoardViewScreen({
 			{mode !== 'edit' ? (
 				<div>
 					<BoardViewHeader isMyPost={isMyPost} DelMutate={DelMutate} postId={boardViewData?.postId ?? null} />
-					<div role="textbox" contentEditable onKeyDown={} onMouseUp={handleTextSelection}>
-						<p ref={mentionRef} className=" text-body2   placeholder:text-g7 text-g9 w-full px-[3.2rem]" />
+					<div ref={commentFormRef} onMouseUp={handleTextSelection} role="textbox" className="w-full h-5">
+						<p
+							ref={mentionRef}
+							onInput={newCommentHandler}
+							contentEditable
+							className=" text-body2   placeholder:text-g7 text-g9 w-full px-[3.2rem]"
+						/>
 					</div>
+					<button onClick={newCommentSubmitHandler} className="mt-[2rem]">
+						댓글 제출
+					</button>
 					<BoardContentView boardViewData={boardViewData} viewCheckHandler={viewCheckHandler} />
 					<section className="pt-[1.8rem] pb-[5.4rem] space-y-[1.6rem] ">
 						<div className="flex items-center space-x-[0.4rem]">
@@ -376,7 +275,7 @@ function BoardViewScreen({
 					>
 						<TextInput
 							mode={commentInputMode}
-							value={newComments[NEW_COMMENT_LAST_INDEX]}
+							value=""
 							onChange={newCommentHandler}
 							placeholder="댓글을 입력해 주세요"
 							submitHandler={newCommentSubmitHandler}
