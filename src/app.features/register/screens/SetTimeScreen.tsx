a80e33c5275bf6952f306ab4a51b600b0de173ff
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-10 13:22:18 +0900

    design : register page3 레이아웃 조정
    화면 늘렸을 경우 피그마 디자인과 일치시킴

--- src/app.features/register/screens/SetTimeScreen.tsx
+++ src/app.features/register/screens/SetTimeScreen.tsx
@@ -95,7 +95,7 @@ function SetTimeScreen() {
 				<div className="flex flex-col space-y-[3.2rem]">
 					<div className="space-y-[0.8rem] w-full">
 						<h2 className="text-g6 text-body2">근무요일(복수선택 가능)</h2>
-						<ul className="grid  grid-cols-7  ">
+						<ul className="grid  grid-cols-7 max-w-[31rem] ml-[0.5rem] ">
 							{/* TODO: 간격 화면 크기별로 대응 */}
 							{['6', '0', '1', '2', '3', '4', '5'].map((item, index) => (
 								<li key={index} className="mx-auto">
