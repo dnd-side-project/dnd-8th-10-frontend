16910bfb8a1d6e2f40b30aa34070eea384548f83
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-15 04:00:51 +0900

    design : 체크리스트 scroll 영역 수정(+모바일에서 devider가 달력 위로 올라가는 이슈 처리)

--- src/app.components/Divider.tsx
+++ src/app.components/Divider.tsx
@@ -4,7 +4,7 @@ interface Props {
 	classNames?: string;
 }
 function Divider({ classNames }: Props) {
-	return <div className={`bg-g1 w-[calc(100%+4rem)] -translate-x-[2rem] h-[1.2rem] ${classNames}`} />;
+	return <div className={`bg-g1 w-[calc(100vw+4rem)] max-w-[50rem] mx-[-2rem] h-[1.2rem] ${classNames ?? ''}`} />;
 }
 
 export default Divider;
