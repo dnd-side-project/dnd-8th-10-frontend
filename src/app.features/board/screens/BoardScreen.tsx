81536398e1f3805a4d4c7ddf7040b11458746289
Siby1lA <psb4644@gmail.com>
2023-03-17 19:30:48 +0900

    fix: 게시판 헤더 fixed

--- src/app.features/board/screens/BoardScreen.tsx
+++ src/app.features/board/screens/BoardScreen.tsx
@@ -11,25 +11,27 @@ import BoardPreview from '../components/BoardPreview';
 function BoardScreen() {
 	const router = useRouter();
 	return (
-		<div className="">
-			<header className="w-full h-[5.6rem] flex items-center justify-between">
+		<div>
+			<header className="bg-w fixed w-full max-w-[50rem] h-[5.6rem] -translate-x-[2rem] px-[2rem] flex items-center justify-between z-[151]">
 				<span className="text-g10 text-subhead4 ml-[0.1rem]">편의점 게시판</span>
 				<button type="button" onClick={() => router.push(SERVICE_URL.boardSearch)}>
 					<SearchIcon />
 				</button>
 			</header>
-			<BoardPreviewSlider />
-			<Divider />
-			<div className="my-[2.4rem]">
-				<BoardCategorySlider />
-			</div>
-			<div className="mb-[5.6rem]">
-				<BoardPreview />
-			</div>
-			<div className="flex justify-end max-w-[50rem] -translate-x-[4rem] w-full fixed bottom-0 mb-[8.1rem] pointer-events-none">
-				<button className="cursor-pointer pointer-events-auto" onClick={() => router.push(SERVICE_URL.boardWrite)}>
-					<WriteIcon />
-				</button>
+			<div className="pt-[5.6rem]">
+				<BoardPreviewSlider />
+				<Divider />
+				<div className="my-[2.4rem]">
+					<BoardCategorySlider />
+				</div>
+				<div className="mb-[5.6rem]">
+					<BoardPreview />
+				</div>
+				<div className="flex justify-end max-w-[50rem] -translate-x-[4rem] w-full fixed bottom-0 mb-[8.1rem] pointer-events-none">
+					<button className="cursor-pointer pointer-events-auto" onClick={() => router.push(SERVICE_URL.boardWrite)}>
+						<WriteIcon />
+					</button>
+				</div>
 			</div>
 		</div>
 	);
