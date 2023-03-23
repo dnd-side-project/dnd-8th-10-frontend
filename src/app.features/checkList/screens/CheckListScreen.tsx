1e4de4bad10d483723dd2444ed491332c32572f7
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-03-23 18:49:59 +0900

    feat : 체크리스트. 신규회원 안내 메시지 박스 띄우기

--- src/app.features/checkList/screens/CheckListScreen.tsx
+++ src/app.features/checkList/screens/CheckListScreen.tsx
@@ -85,6 +85,7 @@ function CheckListScreen({
 	const [newTodo, setNewTodo] = useState<string>('');
 	const [selectedDateIdx, setSelectedDateIdx] = useState<number>(day === 6 ? 0 : day);
 	const [editContent, setEditContent] = useState<string>('');
+	const [isNewbie, setIsNewbie] = useState(getCookie(COOKIE_KEY.IS_NEWBIE));
 	const calcWeek = () => {
 		const { curMonthLastDate } = getCurMonthLastDayInfo(year, month);
 		const { prevMonthLastDate, prevMonthLastDay } = getPrevMonthLastDayInfo(year, month);
@@ -202,9 +203,9 @@ function CheckListScreen({
 		const [resYear, resMonth] = searchDate.split('-');
 		return `${+resYear}년 ${+resMonth}월`;
 	};
-	//	document.cookie = `${COOKIE_KEY.ACCESS_TOKEN}=; max-age=-1 ;path=/;`;
+
 	// TODO: mx-[-2rem] 속성없이 divider 꽉채우기
-	console.log(checklist, getCookie(COOKIE_KEY.IS_NEWBIE));
+
 	return (
 		<>
 			<Header title="내 할일 점검" />
@@ -244,6 +245,25 @@ function CheckListScreen({
 
 				<div className=" overflow-x-hidden px-[2rem]    relative">
 					<Divider />
+					{getCookie(COOKIE_KEY.IS_NEWBIE) && (
+						<div className="w-full bg-g1 rounded-[0.8rem]">
+							<div className="mx-auto space-y-[0.8rem]  mt-[2.4rem] p-[1.6rem] text-subhead2  text-g8">
+								<p className="text-start whitespace-pre-wrap">
+									{`내 할일 점검에서 잊지않게 업무를 체크해보세요!\n나의 업무에 맞게 항목을 수정, 삭제할 수 있습니다.`}
+								</p>
+								<button
+									onClick={() => {
+										document.cookie = `${COOKIE_KEY.IS_NEWBIE}=false; max-age=-1 ;path=/;`;
+										setIsNewbie(false);
+									}}
+									className="text-primary w-full text-end"
+								>
+									닫기
+								</button>
+							</div>
+						</div>
+					)}
+
 					{isChecklistFetched && isWorkDay ? (
 						<div className=" text-subhead2 space-y-[1.6rem] py-[2.4rem]  relative ">
 							<div className="w-full bg-white ">
