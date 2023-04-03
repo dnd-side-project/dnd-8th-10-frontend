3f92bcef6229400b573c32daaa2597c936c31294
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-03 11:48:34 +0900

    refactor: #mypage 내가쓴글 데이터 ssr로 로드

--- pages/mypage/post.tsx
+++ pages/mypage/post.tsx
@@ -1,23 +1,29 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import { InitialDataFunction, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios, { AxiosResponse } from 'axios';
+import { GetServerSideProps } from 'next';
+import React, { useEffect } from 'react';
 import TitleHead from 'src/app.components/TitleHead';
+import { PostDatas } from 'src/app.features/board/types';
 import MyPostScreen from 'src/app.features/mypage/screens/MyPostScreen';
 import { getUserPost } from 'src/app.modules/api/user';
 
-function myPost() {
-	const { data, refetch, isLoading } = useQuery(['user', 'post'], getUserPost, {
-		select: (res) => res.data.data,
-		onSuccess: (res) => {
-			console.log(res);
-		},
-		onError: (error) => {
-			console.log(error);
-		},
-	});
+interface Props {
+	serverData?: any;
+}
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+	const { req } = context;
+	const API_URL = 'https://wise24life.shop/api/myPost';
+	const token = req.cookies.wiseat; // 쿠키에서 토큰 추출
+	const headers = { Authorization: `Bearer ${token}` }; // 헤더 설정
+	const { data } = await axios.get(API_URL, { headers }); // API 요청
+	return { props: { serverData: data } };
+};
+
+function myPost({ serverData }: Props) {
 	return (
 		<>
 			<TitleHead title="내가 쓴 글" />
-			<MyPostScreen posts={data} />
+			<MyPostScreen posts={serverData?.data} />
 		</>
 	);
 }
