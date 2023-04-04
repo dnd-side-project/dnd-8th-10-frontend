fce073755381d153d82910360a15548bd8b59c6b
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-05 00:03:19 +0900

    refactor : #detail board ssr 함수 작성

--- pages/board/view/[id].tsx
+++ pages/board/view/[id].tsx
@@ -1,5 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import type { NextPage } from 'next';
+import axios from 'axios';
+import type { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import TitleHead from 'src/app.components/TitleHead';
 import { boardDelete } from 'src/app.features/board/api';
@@ -14,7 +15,20 @@ import BoardViewScreen from 'src/app.features/board/screens/BoardViewScreen';
 import useBoardView from 'src/app.modules/hooks/board/useBoardView';
 import useUser from 'src/app.modules/hooks/user/useUser';
 
-const ViewPage: NextPage = () => {
+interface Props {
+	serverData: any;
+}
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+	const { req } = context;
+	const API_URL = 'https://wise24life.shop/api/myPost';
+
+	const { boardView, token } = req.cookies;
+	const headers = { Authorization: `Bearer ${token}`, BoardView: boardView }; // 헤더 설정
+	const { data } = await axios.get(API_URL, { headers }); // API 요청
+	return { props: { serverData: data } };
+};
+
+const ViewPage = ({ serverData }: Props) => {
 	const router = useRouter();
 	const { id } = router.query;
 	const queryClient = useQueryClient();
@@ -79,11 +93,11 @@ const ViewPage: NextPage = () => {
 
 	return (
 		<>
-			<TitleHead title={boardViewData?.title ?? ''} />
+			<TitleHead title={boardViewData?.title ?? serverData?.title ?? ''} />
 			<BoardViewScreen
 				userData={userData}
 				boardCheckPerson={boardCheckPerson}
-				boardViewData={boardViewData}
+				boardViewData={boardViewData ?? serverData}
 				DelMutate={DelMutate}
 				ViewCheckMutate={ViewCheckMutate}
 				PostCommentMutate={PostCommentMutate}
