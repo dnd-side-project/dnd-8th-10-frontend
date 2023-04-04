a3895b4f5758b5e2b70c3b2c2364ecf47319eb78
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-05 00:04:10 +0900

    refactor: middleware 도입

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest, res: NextResponse) {
 	console.dir('미들웨어', req);
-	return res;
+	return NextResponse.next();
 }
-export const config = {
-	matcher: '/',
-};
+export const config = {};
