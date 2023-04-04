f68cc70b8cfeaedf16f277fe1a01802e04a44e11
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-05 04:58:30 +0900

    chore : 되돌리다

--- src/app.modules/api/auth.ts
+++ src/app.modules/api/auth.ts
@@ -6,7 +6,7 @@ import client from './client';
 export const oauth2 = async (code: string) => {
 	const isLocal = Boolean(process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI?.includes('localhost'));
 	console.log('isLocal', isLocal);
-	const res = await axios.get(`/oauth/token?code=${code}&isLocal=${isLocal}`);
+	const res = await client.get(`/oauth/token?code=${code}&isLocal=${isLocal}`);
 	return res;
 };
 
