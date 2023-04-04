b1eff373fa18732c209da122a20021cdff75b18b
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-04-05 03:44:40 +0900

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server';
-
-export function middleware(req: NextRequest, res: NextResponse) {
-	console.dir('미들웨어', req);
-	return NextResponse.next();
-}
-export const config = {};
